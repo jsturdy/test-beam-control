41df7aae950faa5a7323dc2d405f6cd17299114b
Thomas Lenzi <tlenzi@ulb.ac.be>
2014-11-23 18:41:55 +0100

    Autosave 11-23-2014 Nov-11-1416764515

--- src/save.sh
+++ src/save.sh
@@ -1,5 +1,6 @@
 #!/bin/bash        
-now=$(date +"%m-%d-%Y")
+now=$(date +"%m-%d-%Y %h-%m-%s")
+echi ${now}
 git ls-files --deleted -z | xargs -0 git rm --cached
 git add .
 git commit -m "Autosave $now"
